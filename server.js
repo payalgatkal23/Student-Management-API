22203f12eb4d58ffa44d1fb990f863635581f4f7
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-23 21:36:20 +0530

    update

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.get("/readuser", async (req, res) => {
 app.delete("/deleteuser/:name", async (req, res) => {
   res.send(del);
   const del = await student.deleteOne({ name: req.params.name });
+   res.send(del);
 });
 
 //update
