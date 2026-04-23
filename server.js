eae4fe2024c0bee7166e8f902239941ade6ac4f3
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-23 21:43:41 +0530

    updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 mongoose
   .connect("mongodb://localhost:27017/crudapp")
   .then(() => {
-    console.log("Connect to MongoDB");
+    console.log("Connected to MongoDB");
   })
   .catch((error) => {
     console.log("Error Connecting to MongoDB:", error);
