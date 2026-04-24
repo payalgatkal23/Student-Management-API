349233655dd84660a430dd12eaad4df6582f4dfc
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-24 13:29:10 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,15 @@
 const User = require("../models/user");
+
+// CREATE
+exports.createUser = async (req, res) => {
+  try {
+    if (!req.body.name) {
+      return res.json({ message: "Name is required" });
+    }
+
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.json({ message: "Error creating user" });
+  }
+};
