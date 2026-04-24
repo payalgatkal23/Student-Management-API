5e374ee1013dffb1f8c39bbaa2fd241602a24982
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-24 13:30:19 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,3 +13,51 @@ exports.createUser = async (req, res) => {
     res.json({ message: "Error creating user" });
   }
 };
+
+// READ
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.json({ message: "Error fetching users" });
+  }
+};
+
+// DELETE
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.json({ message: "Error deleting user" });
+  }
+};
+
+// UPDATE
+exports.updateUser = async (req, res) => {
+  try {
+    if (!req.body.name) {
+      return res.json({ message: "Name is required" });
+    }
+
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!user) {
+      return res.json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.json({ message: "Error updating user" });
+  }
+};
