48d05525d28ed9bd0cb64454cde39f3099442bbc
payalgatkal23 <payalgatkal50@gmail.com>
2026-04-24 13:33:24 +0530

    updated

---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+router.post("/users", userController.createUser);
+router.get("/users", userController.getUsers);
+router.delete("/users/:id", userController.deleteUser);
+router.put("/users/:id", userController.updateUser);
+
+module.exports = router;
